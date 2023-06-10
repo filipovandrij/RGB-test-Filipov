d94ca63cead82fe1d81615b1bc21440e5bc5256f
Filipov <andrijfilipov73@gmail.com>
2023-06-10 19:53:04 +0300

    add  AdditionalInfo and make style

--- src/component/AdditionalInfo/AdditionalInfo.jsx
+++ src/component/AdditionalInfo/AdditionalInfo.jsx
@@ -3,7 +3,35 @@ import "./AdditionalInfo.scss";
 const AdditionalInfo = () => {
   return (
     <>
-      <div>sfdg</div>
+      <div className="additiona_info">
+        <div className="additional_card">
+          <img src="/images/img_mobile/gift_box.svg" alt="gift_box" />
+          <div className="text_box">
+            <h3 className="bonus">Бонус за регистрацию</h3>
+            <p className="mobile_text">
+              PDF-файл "5 преимуществ профессии веб-дизайнера"{" "}
+            </p>
+            <p className="desktop_text">
+              PDF-файл "5 преимуществ профессии фронтенд разработчика"{" "}
+            </p>
+          </div>
+        </div>
+        <div className="additional_card">
+          <img
+            src="/images/img_mobile/man_with_laptop.svg"
+            alt="man_with_laptop"
+          />
+          <div className="text_box">
+            <h3 className="man_name">
+              Кирилл <span>Касатонов</span>
+            </h3>
+            <p>
+              6 лет коммерческого опыта с такими компаниями как Mercedes-Benz и
+              другими крупными корпорациями
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
